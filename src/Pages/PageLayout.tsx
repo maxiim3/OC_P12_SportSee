48a7f39df4bc658e257888ad106542e20d45d806
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-01-10 15:54:42 +0100

    add animation onload

--- src/Pages/PageLayout.tsx
+++ src/Pages/PageLayout.tsx
@@ -1,29 +1,39 @@
-import React, {createContext} from "react"
+import React, {createContext, useEffect, useState} from "react"
 import {Header} from "../Components/Header"
 import {SideBar} from "../Components/SideBar"
 import {Outlet, useLoaderData} from "react-router-dom"
 import {mockUser} from "../Mock/mockUser"
-import {Loader} from "../Components/Loader"
 import {IUser} from "../Interface/IUser"
+import {Loader} from "../Components/Loader"
 
 export const UserContext = createContext(mockUser)
 
 // todo add documentation
 export function PageLayout() {
+	const [isLoading, setIsLoading] = useState(true)
 	const user = useLoaderData()! as IUser
+
+	useEffect(() => {
+		setIsLoading(!user)
+	}, [user])
+
+	useEffect(() => {
+		const loader = document.getElementById("loader-container") as HTMLElement
+		if (user && !isLoading) {
+			loader.dataset.isVisible = "false"
+		}
+	}, [isLoading])
+
+	if (isLoading) return <Loader />
 
 	return (
 		<>
 			<Header />
 			<main className={"main"}>
 				<SideBar />
-				{!user ? (
-					<Loader />
-				) : (
-					<UserContext.Provider value={user}>
-						<Outlet />
-					</UserContext.Provider>
-				)}
+				<UserContext.Provider value={user}>
+					<Outlet />
+				</UserContext.Provider>
 			</main>
 		</>
 	)
